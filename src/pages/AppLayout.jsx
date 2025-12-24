89a87b133fb5ed459bfa5e64ee10d31f635deb0b
Ali-gunner <ali.ranjbar.lut2024@gmail.com>
2025-12-24 14:49:23 +0200

    Implemented App layout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,10 @@
-import AppNav from "../components/AppNav";
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 
 export default function AppLayout() {
   return <div className={styles.app}>
-    <AppNav />
-    <p>App</p>
+    <Sidebar />
+    <Map />
   </div>
 }
